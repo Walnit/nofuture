b8ac59d5718b97aeb00003f2359bcc5c0285a066
Your Name <you@example.com>
2024-06-20 19:13:30 +0800

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import CourseInfo from './Component/CourseInfo'
 import ExperienceItem from './Component/ExperienceItem'
+import Middlesections from './Component/Middlesections'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <CourseInfo/>
+    <CourseInfo
+    content1={"Home > Diploma Courses> Natural, Physical, Chemical & Mathematical Sciences >" }
+    title={"Pharmaceutical Science"}
+    subtitle={"Learn the biology and chemistry behind creating new drugs and medicines."}
+    />
+    <Middlesections/>
     <ExperienceItem date="13 Mar" content="NYP Open House"/>
     <br />
     <ExperienceItem date="14 Mar" content="Pharmaceutical Conferences"/>
--- frontend/src/Component/CourseInfo.jsx
+++ frontend/src/Component/CourseInfo.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-export default function CourseInfo() {
+export default function CourseInfo({content1, title, subtitle}) {
   return (
   <>
-  <div className='parent1' style={{padding:'20px'}}>
-    <p>{"Home > Diploma Courses> Natural, Physical, Chemical & Mathematical Sciences > "}<span style={{color:'#2400FF'}}>{"Pharmaceutical Science"}</span></p>
-    <p style={{background:"linear-gradient(to bottom, Transparente 0%,Transparente 50%,red 50%,red 100%)"}}><h1>{"Pharmaceutical Science"} </h1></p>
-    <p>{"Learn the biology and chemistry behind creating new drugs and medicines."}</p>
+  <div style={{padding:'20px'}}>
+    <p>{content1}<span style={{color:'#2400FF'}}>{title}</span></p>
+    <p style={{background:"linear-gradient(to bottom, Transparente 0%,Transparente 50%,red 50%,red 100%)"}}><h1>{title} </h1></p>
+    <p>{subtitle}</p>
     </div>
       </>
   )
